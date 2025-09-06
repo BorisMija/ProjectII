40cf997280c0c4abf39beeb73ed46b479cb95f72
BerzanIrma <irmabia2005@gmail.com>
2025-09-06 18:09:46 +0300

    Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,40 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-4">
-        <Button>Click Me</Button>
+      <UserButton 
+        afterSignOutUrl="/"
+        appearance={{
+          elements: {
+            userButtonPopoverCard: {
+              display: "flex",
+              flexDirection: "column",
+            },
+            userButtonPopoverActions: {
+              display: "flex",
+              flexDirection: "column",
+            },
+            userButtonPopoverActionButton: {
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+            },
+          },
+        }}
+        userProfileProps={{
+          appearance: {
+            elements: {
+              navbar: {
+                display: "none",
+              },
+              navbarMobileMenuButton: {
+                display: "none",
+              },
+            },
+          },
+        }}
+      />
     </div>
   )
 }
